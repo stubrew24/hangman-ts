12e1783a1ff0a21edb3dd5b17798f1d24690d20c
Stuart Sewell <hello@stuartsewell.dev>
2020-02-16 13:15:58 +0000

    Revert to original word gen

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import Controller from "./Controller";
 import Model from "./Model";
 import View from "./View";
-const { RAPID_API_KEY } = process.env;
 
-console.log("API_KEY:", RAPID_API_KEY);
-const app = new Controller(new Model(), new View());
+new Controller(new Model(), new View());
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,28 @@
 const path = require("path");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
   entry: "./dist/index.js",
   mode: "production",
-  optimization: {
-    minimizer: [
-      new UglifyJsPlugin({
-        parallel: true
-      })
-    ]
-  },
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "public")
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      PRODUCTION: JSON.stringify(true),
+      VERSION: JSON.stringify("5fa3b9"),
+      BROWSER_SUPPORTS_HTML5: true,
+      TWO: "1+1",
+      "typeof window": JSON.stringify("object"),
+      "process.env": {
+        RAPID_API_KEY: JSON.stringify(
+          "895c54ea64msh6c37d0108d1ee19p11a0e7jsn7d9c264f6ff6"
+        )
+      }
+    })
+  ],
+  node: {
+    fs: "empty"
   }
 };
